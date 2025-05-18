c22fb35363c89d06f83a6c97c6cd7a14b874e6ae
Wolfkiller42 <147341858+Wolfkiller42@users.noreply.github.com>
2025-05-18 12:40:40 -0400

    Delete src directory

--- src/pages/About.jsx
+++
@@ -1,7 +0,0 @@
-const About = () => (
-  <div>
-    <h1>About This Project</h1>
-    <p>This app was built for INST 377 using React, Supabase, and the NumVerify API.</p>
-  </div>
-);
-export default About;
--- src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => (
-  <div>
-    <h1>Welcome to Phone Verifier 📞</h1>
-    <p>Use this app to check phone number validity and learn about their origin.</p>
-  </div>
-);
-export default Home;
